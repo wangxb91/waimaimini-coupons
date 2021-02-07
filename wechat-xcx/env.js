f5840e771149ecfc259b8c46dad0e4ea5b8d2c38
wxb <519349328@qq.com>
2021-02-07 16:41:58 +0800

    update

---
+++ wechat-xcx/env.js
@@ -0,0 +1,24 @@
+export default {
+  // 微信云函数配置
+ // wxCloudEnv: "demo-4g5zv6qr65613d1v",
+  // 拼多多appid
+  pddAppId: "wx32540bd863b27570",
+  // 有趣的appid
+  otherMiniPrograms: [
+    {
+      icon: "/images/rubbish.png",
+      name: "垃圾分类神探",
+      appId: "wxe5b0521cff7d1331",
+      path: "/pages/ai/index",
+    },
+  ],
+  // 首页子分类进去的详情页顶部banner图片地址
+  subTypeBannerImage: {
+    shot:
+      "cloud://waimaiqixiangtai-6f82fsgefc4719b.7761-waimaiqixiangtai-6f82fsgefc4719b-1304942998/banner/s-hot.jpg",
+    s19:
+      "cloud://waimaiqixiangtai-6f82fsgefc4719b.7761-waimaiqixiangtai-6f82fsgefc4719b-1304942998/banner/s-19.jpg",
+    spp:
+      "cloud://waimaiqixiangtai-6f82fsgefc4719b.7761-waimaiqixiangtai-6f82fsgefc4719b-1304942998/banner/s-pinpai.jpg",
+  },
+};
